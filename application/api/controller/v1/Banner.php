08745cc09d3b508821417ab861d52f406e20a7e5
张雷雷 <zhangleilei@wecash.net>
2018-03-04 16:28:13 +0800

    Initial commit

---
+++ application/api/controller/v1/Banner.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wecash
+ * Date: 04/03/2018
+ * Time: 16:22
+ */
+
+namespace app\api\controller\v1;
+
+
+class Banner
+{
+
+    /**
+     * 获取指定Id的banner信息
+     * @url /banner/:id
+     * @http GET
+     * @id banner的Id
+     */
+    public function getBanner($id)
+    {
+
+        return;
+    }
+
+}
---
+++ public/info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wecash
+ * Date: 04/03/2018
+ * Time: 13:21
+ */
+
+phpinfo();
